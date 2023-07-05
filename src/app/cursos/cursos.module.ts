efb6ccf679140795517720fe8b4460b702df444a
lindembergs <lindembergparaiba@gmail.com>
2023-07-05 00:19:19 -0300

    component curso-detalhe criado, o componente é importado e declarado no cursos módule automáticamente.

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 
 
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursosDetalheComponent
 
   ],
   imports: [
